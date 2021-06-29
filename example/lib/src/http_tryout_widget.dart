8582c66a65807dd0a5ee7a3fa0ad2e40976c20e2
Goncalo Silva (CTW) <Goncalo.Silva@ctw.bmwgroup.com>
2021-06-29 11:24:30 +0100

    wip

--- example/lib/src/http_tryout_widget.dart
+++ example/lib/src/http_tryout_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:airwatch_socket_workaround/airwatch_socket_workaround.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -19,83 +21,92 @@ class _HttpTryoutWidgetState extends State<HttpTryoutWidget> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        buildHttpGetWidget('HTTP GET', (endpoint) async {
-          AirWatchHttpWorkAround httpWorkAroundClient =
-              AirWatchWorkAroundFactory.getInstance();
-          var request = dartHttp.Request("GET", Uri.parse(endpoint))
-            ..headers.addAll({'content-type': 'application/json'});
-          var response = await httpWorkAroundClient.doRequest(request);
-          return Stream.value(response.body);
-        }),
-        Padding(
-          padding: EdgeInsets.all(8),
-          child: Divider(
-            height: 8.0,
-          ),
-        ),
-        buildHttpGetWidget('Websocket echo', (endpoint) async {
+        SendHttpRequestWidget('Http request',wsEndpoint,),
+        /*buildHttpGetWidget('Websocket echo', (endpoint) async {
           var session =
               await AirWatchWorkAroundFactory.getInstanceSocketSession(
                   endpoint);
           var stream = session.receiveBroadcastStream();
           session.send('hello');
           return stream;
-        }),
+        }),*/
       ],
     );
   }
+}
 
-  Widget buildHttpGetWidget(
-      String title, Future<Stream<String>> doRequest(String endpoint)) {
-    return Flex(
-      direction: Axis.vertical,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            title,
-            style: Theme.of(context).textTheme.headline4,
-          ),
-        ),
-        Flex(
-          direction: Axis.horizontal,
-          children: [
-            Flexible(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextField(
-                  onChanged: (value) {
-                    wsEndpoint = value;
-                  },
-                  controller: TextEditingController()..text = wsEndpoint,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), hintText: 'endpoint'),
+class SendHttpRequestWidget extends StatefulWidget {
+  final String _title;
+  final String _endpoint;
+
+  const SendHttpRequestWidget(this._title, this._endpoint, {Key key})
+      : super(key: key);
+
+  @override
+  _SendHttpRequestWidgetState createState() => _SendHttpRequestWidgetState();
+}
+
+class _SendHttpRequestWidgetState extends State<SendHttpRequestWidget> {
+  final StreamController<String> responseStream = StreamController();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<String>(
+        stream: responseStream.stream,
+        builder: (context, snapshot) {
+          return Flex(
+            direction: Axis.vertical,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  widget._title,
+                  style: Theme.of(context).textTheme.headline4,
                 ),
               ),
-            ),
-            MaterialButton(
-              child: Text('Send'),
-              onPressed: () async {
-                setState(() {
-                  responseText = '';
-                });
+              Flex(
+                direction: Axis.horizontal,
+                children: [
+                  Flexible(
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                      child: TextField(
+                        onChanged: (value) {
+                          responseStream.add(value);
+                        },
+                        controller: TextEditingController()..text = widget._endpoint,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(), hintText: 'endpoint'),
+                      ),
+                    ),
+                  ),
+                  MaterialButton(
+                    child: Text('Send'),
+                    onPressed: () async {
+                      setState(() {
+                        //responseText = '';
+                      });
 
-                var response = await doRequest(wsEndpoint);
-                response.listen((event) {
-                  setState(() {
-                    responseText = event;
-                  });
-                });
-              },
-            )
-          ],
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 8.0),
-          alignment: AlignmentDirectional.centerStart,
-          child: Text('response was $responseText'),
-        )
-      ],
-    );
+                      AirWatchHttpWorkAround httpWorkAroundClient =
+                          AirWatchWorkAroundFactory.getInstance();
+                      var request = dartHttp.Request(
+                          "GET", Uri.parse(widget._endpoint))
+                        ..headers.addAll({'content-type': 'application/json'});
+                      var response =
+                          await httpWorkAroundClient.doRequest(request);
+                      responseStream.add(response.body);
+                    },
+                  )
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                alignment: AlignmentDirectional.centerStart,
+                child: Text('response was $snapshot'),
+              )
+            ],
+          );
+        });
   }
 }
